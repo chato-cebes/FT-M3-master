5eb5cf3dbcf349d7661393ff6c47f58aa6b078a3
chato-cebes <javancebes@gmail.com>
2023-01-11 18:07:23 -0300

    First Commit

---
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -0,0 +1,31 @@
+const { stdout } = require("process");
+const process = require("process");
+const commands = require("./commands/index.js");
+
+
+function print(output){
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+
+}
+
+function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", (data)=>{
+    const args = data.toString().trim().split(" ");
+    const cmd = args.shift();
+    if (commands.hasOwnProperty(cmd)){
+      commands[cmd](print, args.join(" "));
+    }else{
+      print(`command not found: ${cmd}`);
+    }
+  })
+
+}
+
+
+bash();
+module.exports = {
+  print,
+  bash,
+};
